aa46a1b284fe574b6dc24113c0d0d40621917200
tanvir626 <57144266+tanvir626@users.noreply.github.com>
2022-06-30 21:40:49 +0600

    Adding Employee info and address table

--- Database/SignDB.sql
+++ Database/SignDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2022 at 05:22 PM
+-- Generation Time: Jun 30, 2022 at 05:39 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `customer_address` (
-  `Address_ID` int(8) NOT NULL,
+  `CAddress_ID` int(8) NOT NULL,
   `Name` int(50) NOT NULL,
   `District` int(20) NOT NULL,
   `Division` int(20) NOT NULL,
@@ -44,12 +44,45 @@ CREATE TABLE `customer_address` (
 CREATE TABLE `customer_info` (
   `CustomerID` int(8) NOT NULL,
   `Name` text NOT NULL,
-  `AddressID` int(8) NOT NULL,
+  `CAddressID` int(8) NOT NULL,
   `Mobile` int(13) NOT NULL,
   `Mobile-2` int(13) NOT NULL,
   `Email` varchar(22) NOT NULL,
   `Comapany` varchar(20) NOT NULL,
   `Designation` varchar(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_address`
+--
+
+CREATE TABLE `employee_address` (
+  `EAddress_ID` int(8) NOT NULL,
+  `Name` int(50) NOT NULL,
+  `District` int(20) NOT NULL,
+  `Division` int(20) NOT NULL,
+  `Postal_Code` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_info`
+--
+
+CREATE TABLE `employee_info` (
+  `Employee_ID` int(8) NOT NULL,
+  `Name` text NOT NULL,
+  `EAddressID` int(8) NOT NULL,
+  `Mobile` int(13) NOT NULL,
+  `Mobile-2` int(13) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `Company` varchar(20) NOT NULL,
+  `Designation` text NOT NULL,
+  `Salary` int(6) NOT NULL,
+  `DOB` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -60,13 +93,25 @@ CREATE TABLE `customer_info` (
 -- Indexes for table `customer_address`
 --
 ALTER TABLE `customer_address`
-  ADD PRIMARY KEY (`Address_ID`);
+  ADD PRIMARY KEY (`CAddress_ID`);
 
 --
 -- Indexes for table `customer_info`
 --
 ALTER TABLE `customer_info`
   ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `employee_address`
+--
+ALTER TABLE `employee_address`
+  ADD PRIMARY KEY (`EAddress_ID`);
+
+--
+-- Indexes for table `employee_info`
+--
+ALTER TABLE `employee_info`
+  ADD PRIMARY KEY (`Employee_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
